10148e5942a468fa6d07d0aed1bf8ec87c297a9f
Milan Strnad <milan.strnad@thefuntasty.com>
2017-09-10 16:26:56 +0200

    Add View lifecycle pragmamark

--- MVVM-C Scene.xctemplate/___VARIABLE_sceneIdentifier___ViewController.swift
+++ MVVM-C Scene.xctemplate/___VARIABLE_sceneIdentifier___ViewController.swift
@@ -3,7 +3,7 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 //
 
 import UIKit
@@ -13,6 +13,8 @@ protocol ___VARIABLE_sceneIdentifier___ViewControllerInput: class {
 
 final class ___VARIABLE_sceneIdentifier___ViewController: UIViewController {
     var viewModel: ___VARIABLE_sceneIdentifier___ViewModel!
+
+    // MARK: - View lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
