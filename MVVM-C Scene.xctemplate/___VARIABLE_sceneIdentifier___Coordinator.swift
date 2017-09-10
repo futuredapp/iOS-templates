183893212e41e30a02892391231dd1ec709b97bb
Milan Strnad <milan.strnad@thefuntasty.com>
2017-09-10 16:27:42 +0200

    Reduce constant names and Improve codestyle

--- MVVM-C Scene.xctemplate/___VARIABLE_sceneIdentifier___Coordinator.swift
+++ MVVM-C Scene.xctemplate/___VARIABLE_sceneIdentifier___Coordinator.swift
@@ -3,15 +3,15 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 //
 
 import FuntastyKit
 
 private struct ___VARIABLE_sceneIdentifier___Storyboard: StoryboardType {
     static let name = "___VARIABLE_sceneIdentifier___"
-    static let ___VARIABLE_sceneIdentifier___NavigationController = StoryboardReference<___VARIABLE_sceneIdentifier___Storyboard, UINavigationController>(id: "___VARIABLE_sceneIdentifier___NavigationControllerID")
-    static let ___VARIABLE_sceneIdentifier___ViewController = StoryboardReference<___VARIABLE_sceneIdentifier___Storyboard, ___VARIABLE_sceneIdentifier___ViewController>(id: "___VARIABLE_sceneIdentifier___ViewControllerID")
+    static let navigationController = StoryboardReference<___VARIABLE_sceneIdentifier___Storyboard, UINavigationController>(id: "___VARIABLE_sceneIdentifier___NavigationControllerID")
+    static let viewController = StoryboardReference<___VARIABLE_sceneIdentifier___Storyboard, ___VARIABLE_sceneIdentifier___ViewController>(id: "___VARIABLE_sceneIdentifier___ViewControllerID")
 }
 
 protocol ___VARIABLE_sceneIdentifier___CoordinatorInput: Coordinator {
@@ -25,8 +25,9 @@ final class ___VARIABLE_sceneIdentifier___Coordinator: DefaultCoordinator {
 
     init(navigationController: UINavigationController, serviceHolder: ServiceHolder) {
         self.navigationController = navigationController
+        self.viewController = ___VARIABLE_sceneIdentifier___Storyboard.viewController.instantiate()
+
         self.serviceHolder = serviceHolder
-        self.viewController = ___VARIABLE_sceneIdentifier___Storyboard.___VARIABLE_sceneIdentifier___ViewController.instantiate()
     }
 
     func start() {
@@ -34,6 +35,7 @@ final class ___VARIABLE_sceneIdentifier___Coordinator: DefaultCoordinator {
             return
         }
         viewController.viewModel = ___VARIABLE_sceneIdentifier___ViewModel(coordinator: self, viewController: viewController)
+
         navigationController.pushViewController(viewController, animated: true)
     }
 
