8211138225faf685bb8dcbe453d24c23f972ce37
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2017-10-06 14:22:44 +0200

    Add modal scene template

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ TEMPLATE_HOME=~/Library/Developer/Xcode/Templates/Custom
 mkdir -p $TEMPLATE_HOME
 
 cp -fR ./MVVM-C\ Scene.xctemplate $TEMPLATE_HOME
+cp -fR ./MVVM-C\ Modal\ Scene.xctemplate $TEMPLATE_HOME
 cp -fR ./Cell.xctemplate $TEMPLATE_HOME
