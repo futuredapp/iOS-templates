ee5141555ff54c269ca1b35e5c5038732e8f668e
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2017-11-05 17:04:06 +0100

    Make installation universal

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ TEMPLATE_HOME=~/Library/Developer/Xcode/Templates/Custom
 
 mkdir -p $TEMPLATE_HOME
 
-cp -fR ./MVVM-C\ Scene.xctemplate $TEMPLATE_HOME
-cp -fR ./MVVM-C\ Modal\ Scene.xctemplate $TEMPLATE_HOME
-cp -fR ./MVVM-C\ Push\ Scene.xctemplate $TEMPLATE_HOME
-cp -fR ./Cell.xctemplate $TEMPLATE_HOME
+find . -type d -iname "*.xctemplate" -print0 | while IFS= read -r -d $'\0' filename; do
+    cp -fR "$filename" $TEMPLATE_HOME
+done
