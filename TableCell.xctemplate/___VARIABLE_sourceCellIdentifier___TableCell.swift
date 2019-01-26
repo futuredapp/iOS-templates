d9233785193f4a3bfeb11a126113dacf3cafdede
Mikoláš Stuchlík <stuchlej@gmail.com>
2019-01-26 16:11:17 +0100

    Initial commit

---
+++ TableCell.xctemplate/___VARIABLE_sourceCellIdentifier___TableCell.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___ using Table View Cellkit Template (v1.0).
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import CellKit
+
+final class ___VARIABLE_sourceCellIdentifier___TableCell: UITableViewCell {
+
+}
+
+extension ___VARIABLE_sourceCellIdentifier___TableCell: CellConfigurable {
+    func configure(with model: ___VARIABLE_sourceCellIdentifier___TableCellModel) {
+    }
+}
---
+++ TableCell.xctemplate/___VARIABLE_sourceCellIdentifier___TableCellModel.swift
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___ using Table View Cellkit Template (v1.0).
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import CellKit
+
+struct ___VARIABLE_sourceCellIdentifier___TableCellModel {
+
+}
+
+extension ___VARIABLE_sourceCellIdentifier___TableCellModel: ReusableCellConvertible {
+    typealias Cell = ___VARIABLE_sourceCellIdentifier___TableCell
+
+    var cellHeight: CGFloat {
+        return UITableViewAutomaticDimension
+    }
+}
