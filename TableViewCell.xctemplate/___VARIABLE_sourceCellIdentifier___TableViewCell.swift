7e8b68497fa4ce499e7549d253e1b25c2b35116e
Mikoláš Stuchlík <stuchlej@gmail.com>
2019-02-07 14:20:36 +0100

    Add word View to Table View Cell Template

---
+++ TableViewCell.xctemplate/___VARIABLE_sourceCellIdentifier___TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___ using CellKit Table View Cell Template (v1.0).
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import CellKit
+
+final class ___VARIABLE_sourceCellIdentifier___TableViewCell: UITableViewCell {
+
+}
+
+extension ___VARIABLE_sourceCellIdentifier___TableViewCell: CellConfigurable {
+    func configure(with model: ___VARIABLE_sourceCellIdentifier___TableViewCellModel) {
+    }
+}
