9290107f1592506cbdfa19b542dbb68d10a4480a
Mikoláš Stuchlík <stuchlej@gmail.com>
2019-02-08 10:39:01 +0100

    Implement header footer template

---
+++ CellKit/TableViewHeaderFooter.xctemplate/___VARIABLE_sourceCellIdentifier___TableViewHeaderFooterModel.swift
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___ using CellKit Table View Header Footer Template (v1.0).
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import CellKit
+
+struct ___VARIABLE_sourceCellIdentifier___TableViewHeaderFooterModel {
+
+}
+
+extension ___VARIABLE_sourceCellIdentifier___TableViewHeaderFooterModel: SupplementaryViewModel, CellConvertible {
+    typealias Cell = ___VARIABLE_sourceCellIdentifier___TableViewHeaderFooter
+
+    var cellHeight: Double {
+        return 45.0
+    }
+}
