430a7c10b484da8a8dffc724cfd2a676da54ff9a
Mikoláš Stuchlík <mikolas.stuchlik@thefuntasty.com>
2019-05-29 15:39:24 +0200

    Revert "Add template for modal"
    This reverts commit b81d5c43e754f7697ee0cdeded26adebbec2b005.

--- MVVM-C/MVVM-C Modal Scene.xctemplate/___VARIABLE_sceneIdentifier___Coordinator.swift
+++ MVVM-C/MVVM-C Modal Scene.xctemplate/___VARIABLE_sceneIdentifier___Coordinator.swift
@@ -2,7 +2,7 @@
 //  ___FILENAME___
 //  ___PROJECTNAME___
 //
-//  Created by ___FULLUSERNAME___ on ___DATE___ using ArchitectureKit Modal View Template (v1.0).
+//  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 //
 
