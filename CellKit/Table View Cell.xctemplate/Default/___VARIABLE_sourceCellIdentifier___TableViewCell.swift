7b27a987f5f1554d58a8bc7ff77626d36668c1fb
Adam Bezak <bezoadam95@gmail.com>
2019-10-18 22:34:43 +0200

    add SPM support on CellKit and FTAPIKit

---
+++ CellKit/Table View Cell.xctemplate/Default/___VARIABLE_sourceCellIdentifier___TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___ using CellKit Table View Cell Template (v1.0).
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import CellKit
+import UIKit
+
+final class ___VARIABLE_sourceCellIdentifier___TableViewCell: UITableViewCell {
+
+}
+
+extension ___VARIABLE_sourceCellIdentifier___TableViewCell: CellConfigurable {
+    func configure(with model: ___VARIABLE_sourceCellIdentifier___TableViewCellModel) {
+    }
+}
